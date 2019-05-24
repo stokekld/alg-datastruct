fcffafea7ee0c29b3dd534feca78e7917300d2e1
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-23 21:59:32 -0500

    Many values added  On branch master 	modified:   LinearSearch/main.c

--- LinearSearch/main.c
+++ LinearSearch/main.c
@@ -4,13 +4,11 @@
 
 int main()
 {
-    int value;
-
     int array1[10] = {1, 3, 4, 7, 9, 12, 14, 45, 62, 93};
 
-    value = linearSearch(array1, 10, 14);
-
-    printf("Index of the value: %d\n", value);
+    printf("Index of the value 14: %d\n", linearSearch(array1, 10, 14));
+    printf("Index of the value 62: %d\n", linearSearch(array1, 10, 62));
+    printf("Index of the value 100: %d\n", linearSearch(array1, 10, 100));
 
     return 0;
 }
