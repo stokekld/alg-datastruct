122d0676f5d0193bd3721c5e27d8770bb2f96e0d
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-23 21:53:45 -0500

    Trying the algorithm  On branch master 	modified:   LinearSearch/Makefile 	modified:   LinearSearch/main.c

--- LinearSearch/main.c
+++ LinearSearch/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+#include "lsearch.h"
+
 int main()
 {
-    printf("Helo world\n");
+    int value;
+
+    int array1[10] = {1, 3, 4, 7, 9, 12, 14, 45, 62, 93};
+
+    value = linearSearch(array1, 10, 14);
+
+    printf("Index of the value: %d\n", value);
 
     return 0;
 }
