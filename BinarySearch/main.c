7eaa1076e6eb52b331cfe23fddc2470e3fba5797
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-24 19:43:21 -0500

    Trying algorithm  On branch master 	modified:   BinarySearch/Makefile 	modified:   BinarySearch/main.c

--- BinarySearch/main.c
+++ BinarySearch/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+#include "bsearch.h"
+
 int main()
 {
-    printf("Hello World\n");
+    int array1[10] = {1, 3, 5, 6, 9, 17, 38, 59, 81, 101};
+
+    printf("Index of the value 5: %d\n", binarySearch(array1, 10, 5));
+    printf("Index of the value 59: %d\n", binarySearch(array1, 10, 59));
+    printf("Index of the value 14: %d\n", binarySearch(array1, 10, 14));
 
     return 0;
 }
