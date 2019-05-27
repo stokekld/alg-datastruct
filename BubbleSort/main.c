82087fe05d1bb526ee5ce6bc6f1cc84fb623db90
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-27 14:40:05 -0500

    trying algorithm  On branch master 	modified:   BubbleSort/Makefile 	modified:   BubbleSort/main.c

--- BubbleSort/main.c
+++ BubbleSort/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#include "bsort.h"
+
 int main()
 {
-    printf("Hello World\n");
+    int i, array1[10] = {34, 36, 8, 26, 198, 52, 79, 42, 938, 64};
+
+    bubbleSort(array1, 10);
+
+    for(i = 0; i < 10; i++)
+	printf("%d, ", array1[i]);
+
+    printf("\n");
 
     return 0;
 }
