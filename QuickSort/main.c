e0bda1d76d103b67889933531c6a1a28acf06e61
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-27 18:57:16 -0500

    Trying algorithm  On branch quicksort 	modified:   QuickSort/Makefile 	modified:   QuickSort/main.c

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+#include "qsort.h"
+
 int main()
 {
-    printf("Hello World\n");
+    int i, array1[10] = {1, 5, 23, 456, 5, 3, 76, 12, 64, 5};
+
+    for(i = 0; i < 10; i++)
+	printf("%d, ", array1[i]);
+
+    printf("\n");
+
+    quickSort(array1, 10);
+
+    for(i = 0; i < 10; i++)
+	printf("%d, ", array1[i]);
+
+    printf("\n");
+
 
     return 0;
 }
