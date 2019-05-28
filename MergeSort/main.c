b842e6f7aa313313b87946d71533f64273f4e4d9
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-28 11:59:24 -0500

    Trying algorithm  On branch mergesort 	modified:   MergeSort/Makefile 	modified:   MergeSort/main.c

--- MergeSort/main.c
+++ MergeSort/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+#include "msort.h"
+
 int main()
 {
-    printf("Hello World\n");
+    int i, array1[10] = {4, 55, 1, 46, 874, 26, 96, 45, 86, 7};
+
+    for(i = 0; i < 10; i++)
+	printf("%d, ", array1[i]);
+
+    printf("\n");
+
+    mergeSort(array1, 10);
+
+    for(i = 0; i < 10; i++)
+	printf("%d, ", array1[i]);
+
+    printf("\n");
 
     return 0;
 }
