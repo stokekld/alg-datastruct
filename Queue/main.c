bf542475d33f9fd559842d25a5e5a10d202f4534
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-30 15:40:12 -0500

    Trying functions  On branch queue 	modified:   Queue/Makefile 	modified:   Queue/main.c

--- Queue/main.c
+++ Queue/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#include "queue.h"
+
 int main()
 {
-    printf("Hello World\n");
+    Queue *q = getNewQueue();
+
+    enqueue(q, 723);
+    enqueue(q, 128);
+    enqueue(q, 873);
+
+    printf("%d, ", dequeue(q));
+    printf("%d, ", dequeue(q));
+    printf("%d, ", dequeue(q));
+    printf("%d, ", dequeue(q));
 
     return 0;
 }
