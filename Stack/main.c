8325d158f50d52f5832bb8862d0a2d437e5d0d88
Jesus Flores <joy_warmgun@hotmail.com>
2019-05-30 12:40:46 -0500

    Trying functions  On branch stack 	modified:   Stack/Makefile 	modified:   Stack/main.c

--- Stack/main.c
+++ Stack/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#include "stack.h"
+
 int main()
 {
-    printf("Hello World\n");
+    Stack *stack = getNewStack();
+
+    push(stack, 873);
+    push(stack, 383);
+    push(stack, 312);
+
+    printf("%d\n", pop(stack));
+    printf("%d\n", pop(stack));
+    printf("%d\n", pop(stack));
+    printf("%d\n", pop(stack));
 
     return 0;
 }
